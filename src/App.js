711c112c0bd7aada58433d6caf5216260b8af68c
Guillaume Milan <guillaume.milan@kbrw.fr>
2020-08-12 12:33:23 +0200

    [MINI GAME] Implement all sprites

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ import Vector from './Geometry.js';
 import './App.css';
 
 function App() {
-  const faceAnnimation = new Sprite("sprite-animate", './Amaura.png', new Vector(57, 71), new Vector(1, 0), new Vector(20, 26), 12, "3s", {})
+  const faceAnimation = new Sprite("sprite-animate", './Amaura.png', new Vector(60, 71), new Vector(1, 0), new Vector(20, 26), 12, "3s", {})
+  const profileLeft = new Sprite("amaura-left", './Amaura.png', new Vector(60, 98), new Vector(1, 0), new Vector(20, 28), 12, "3s", {})
+  const backAnimation = new Sprite("amaura-back", './Amaura.png', new Vector(60, 127), new Vector(1, 0), new Vector(20, 27), 12, "3s", {})
+  const profileRight = new Sprite("amaura-right", './Amaura.png', new Vector(60, 156), new Vector(1, 0), new Vector(20, 28), 12, "3s", {})
   return (
-    <div>
+    <div className="Sprites-container">
       <div className="Sprite-holder">
       </div>
-      <div style={faceAnnimation.toCss()}></div>
+      <div className="Sprite" style={faceAnimation.toCss()}></div>
+      <div className="Sprite" style={profileLeft.toCss()}></div>
+      <div className="Sprite" style={backAnimation.toCss()}></div>
+      <div className="Sprite" style={profileRight.toCss()}></div>
     </div>
   );
 }
